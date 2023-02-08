d7fae04f8270e65298c4fa1d83db5edb656a540f
malinatrash <malinatrashh@gmail.com>
2023-02-08 20:10:28 +0800

    Добавил логику создания пользователя при регистрации

--- frontend/src/modules/register/RegisterForm.tsx
+++ frontend/src/modules/register/RegisterForm.tsx
@@ -1,21 +1,65 @@
-import React from 'react'
 import { Button, Input } from 'antd'
-import classes from '../../pages/auth/AuthPage.module.css'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import classes from '../../pages/auth/AuthPage.module.css'
 import Form from '../form/Form'
-import Uploader from './uploader/Uploader'
 
 const RegisterForm = () => {
+	const [name, setName] = useState<string>('')
+	const [username, setUsername] = useState<string>('')
+	const [surname, setSurname] = useState<string>('')
+	const [password, setPassword] = useState<string>('')
+
+	const createUser = () => {
+		return {
+			username: username,
+			password: password,
+			name: name,
+			surname: surname,
+			image: 'https://picsum.photos/200',
+		}
+	}
+
+	const register = () => {
+		let user = createUser()
+		console.log(user)
+	}
+
 	return (
 		<Form title={'Регистрация'}>
 			<div style={{ display: 'flex' }}>
-				<Input placeholder={'Имя'} className={classes.input} />
-				<Input placeholder={'Фамилия'} className={classes.input} />
+				<Input
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					placeholder={'Имя'}
+					className={classes.input}
+				/>
+				<Input
+					value={surname}
+					onChange={(e) => setSurname(e.target.value)}
+					placeholder={'Фамилия'}
+					className={classes.input}
+				/>
 			</div>
-			<Input placeholder={'Имя пользователя'} className={classes.input} />
-			<Input.Password placeholder={'Пароль'} className={classes.input} />
-			<Uploader />
-			<Button className={classes.loginButton} type={'primary'}>
+			<Input
+				value={username}
+				onChange={(e) => setUsername(e.target.value)}
+				placeholder={'Имя пользователя'}
+				className={classes.input}
+			/>
+			<Input.Password
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+				placeholder={'Пароль'}
+				className={classes.input}
+			/>
+
+			{/* <Uploader /> */}
+			<Button
+				className={classes.loginButton}
+				type={'primary'}
+				onClick={register}
+			>
 				<Link to={'/feed'}>Зарегистрироватся</Link>
 			</Button>
 		</Form>
