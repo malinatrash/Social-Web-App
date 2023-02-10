2c9614f3d6247180c1770d69b21917b441708e04
malinatrash <malinatrashh@gmail.com>
2023-02-10 15:30:35 +0800

    Сделал несложную общую для всех полей валидацию. Приложение не даст пользователю зарегистрироваться при пустых полях и покажет уведомление.

--- frontend/src/modules/register/RegisterForm.tsx
+++ frontend/src/modules/register/RegisterForm.tsx
@@ -4,19 +4,34 @@ import { Link } from 'react-router-dom'
 import classes from '../../pages/auth/AuthPage.module.css'
 import Form from '../form/Form'
 import { getCurrentUser, IUser } from '../../model/IUser'
+import FloatingAlert from '../alerts/floating/FloatingAlert'
 
 const RegisterForm = () => {
+	const [regIsAvailable, setRegIsAvailable] = useState<boolean>(false)
+	const [alertIsHidden, setAlertIsHidden] = useState<boolean>(true)
 	const [name, setName] = useState<string>('')
 	const [username, setUsername] = useState<string>('')
 	const [surname, setSurname] = useState<string>('')
 	const [password, setPassword] = useState<string>('')
 
+	const isCorrectField = (field: string) => {
+		if (field != '' && field != null) {
+			setRegIsAvailable(true)
+			return field.trim()
+		}
+		setAlertIsHidden(false)
+		setTimeout(() => {
+			setAlertIsHidden(true)
+		}, 2000)
+		return ''
+	}
+
 	const createUser = () => {
 		let user: IUser = {
-			username: username,
-			password: password,
-			name: name,
-			surname: surname,
+			username: isCorrectField(username),
+			password: isCorrectField(password),
+			name: isCorrectField(name),
+			surname: isCorrectField(surname),
 			image: 'https://picsum.photos/200'
 		}
 		return user
@@ -28,43 +43,49 @@ const RegisterForm = () => {
 	}
 
 	return (
-		<Form title={'Регистрация'}>
-			<div style={{ display: 'flex' }}>
+		<div>
+			<FloatingAlert message={'Заполните все поля'} type={'error'} isHidden={alertIsHidden} />
+			<Form title={'Регистрация'}>
+				<div style={{ display: 'flex' }}>
+					<Input
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+						placeholder={'Имя'}
+						className={classes.input}
+					/>
+					<Input
+						value={surname}
+						onChange={(e) => setSurname(e.target.value)}
+						placeholder={'Фамилия'}
+						className={classes.input}
+					/>
+				</div>
 				<Input
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-					placeholder={'Имя'}
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+					placeholder={'Имя пользователя'}
 					className={classes.input}
 				/>
-				<Input
-					value={surname}
-					onChange={(e) => setSurname(e.target.value)}
-					placeholder={'Фамилия'}
+				<Input.Password
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					placeholder={'Пароль'}
 					className={classes.input}
 				/>
-			</div>
-			<Input
-				value={username}
-				onChange={(e) => setUsername(e.target.value)}
-				placeholder={'Имя пользователя'}
-				className={classes.input}
-			/>
-			<Input.Password
-				value={password}
-				onChange={(e) => setPassword(e.target.value)}
-				placeholder={'Пароль'}
-				className={classes.input}
-			/>
 
-			{/* <Uploader /> */}
-			<Button
-				className={classes.loginButton}
-				type={'primary'}
-				onClick={register}
-			>
-				<Link to={'/feed'}>Зарегистрироватся</Link>
-			</Button>
-		</Form>
+				{/* <Uploader /> */}
+				<Button
+					className={classes.loginButton}
+					type={'primary'}
+					onClick={register}
+				>{
+					regIsAvailable
+						? <Link to={'/feed'}>Зарегистрироватся</Link>
+						: <span>Зарегистрироватся</span>
+				}
+				</Button>
+			</Form>
+		</div>
 	)
 }
 
