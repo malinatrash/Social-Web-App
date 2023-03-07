fc7b535b9fccc10736f47895b5e21469f7840ff2
malinatrash <malinatrashh@gmail.com>
2023-03-07 23:24:10 +0800

    Little changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './modules/app/App'
-import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './modules/app/App'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<BrowserRouter>
